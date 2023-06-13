7a354bcedc5352fdc6841d2ca39161e402c5cd41
sherrerad <135677585+sherrerad@users.noreply.github.com>
2023-06-13 08:32:53 -0400

    Pregunta 2 del mini control realizado

---
+++ pregunta2.py
@@ -0,0 +1,20 @@
+#def es_primo(numero):
+def funcion_desconocida(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+#primos = []
+valores_recogidos = []
+
+for num in range(1, 11):
+    #if es_primo(num):
+    if funcion_desconocida(num):
+        #primos.append(num)
+        valores_recogidos.append(num)
+
+#print(primos)
+print(valores_recogidos)
